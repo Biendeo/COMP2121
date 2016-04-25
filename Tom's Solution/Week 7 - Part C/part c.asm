7f70b06a649d65f44b33555ee24decfb15d0ba12
julianblacket <julian.blacket@gmail.com>
2016-04-26 09:30:41 +1000

    Julian's

--- Tom's Solution/Week 7 - Part C/part c.asm
+++ Tom's Solution/Week 7 - Part C/part c.asm
@@ -8,6 +8,7 @@
 
 .equ patternlength = 8
 .equ timerinterval = 7812
+.equ debounceinterval = 500 ; 10 milliseconds, may need to change
 .equ maxflashes = 6
 .def temp1 = r20
 .def temp2 = r21
@@ -15,6 +16,7 @@
 .def patternlengthtemp = r23
 .def flashcountertemp = r24
 .def activepatternstemp = r16
+.def debouncetemp = r17
 
 .dseg
 pattern: .byte 1
@@ -24,6 +26,9 @@ activepatterns: .byte 1
 ispatterndisplayed: .byte 1
 flashcounter: .byte 1
 timercounter: .byte 2
+debouncecounter: .byte 2
+debounceflag0: .byte 1
+debounceflag1: .byte 1
 
 .cseg
 .org 0x0000
@@ -35,6 +40,8 @@ timercounter: .byte 2
 .org OVF0addr
 	jmp timer0Interrupt
 
+	jmp DEFAULT
+DEFAULT: reti
 
 reset:
 	ldi yl, low(ramend)
@@ -42,153 +49,116 @@ reset:
 	out spl, yl
 	out sph, yh
 
-	; write 0 to counter and pattern
-	ldi yh, high(patternindex<<1)
-	ldi yl, low(patternindex<<1)
-	clr temp1
-	st y, temp1
-	ldi yh, high(pattern<<1)
-	ldi yl, low(pattern<<1)
-	st y, temp1
-	ldi yh, high(flashcounter<<1)
-	ldi yl, low(flashcounter<<1)
-	st y, temp1
-
-	; set prescaling value of the timer
-	ldi temp1, 0
-	out TCCR0A, temp1
-	ldi temp1, 0b00000010
-	out TCCR0B, temp1
-	ldi temp1, 1<<TOIE0
-	sts TIMSK0, temp1
-
 	; set port b as input 
 	clr temp1
 	out DDRB, temp1
-	out PORTB, temp1
-	out PORTC, temp1
+
 	; set port c as output
 	ser temp1
 	out DDRC, temp1
 
-	; set as falling edge for int1 and int0
-	ldi yh, high(EICRA)
-	ldi yl, low(EICRA)		
-	ldi temp1, ISC01
-	ori temp1, ISC11
-	st y, temp1 
-
-	; enable the interrupts
-	in temp1, EIMSK
-	ori temp1, (1<<INT0)
-	ori temp1, (1<<INT1)
-	out EIMSK, temp1
-
-	sei
-	rjmp wait
+	jmp main
 	
 setOne:
 	push temp1
 	in temp1, SREG	
 	push temp1
-	push temp2
 	push patterntemp
 	push patternlengthtemp
 
-	ldi temp1, 0xff
-	delay: ; need to find a better way to handle buttons
-		dec temp1
-		cpi temp1, 0
-		brne delay
-	ldi temp1, 0xff
-	delay1: 
-		dec temp1
-		cpi temp1, 0
-		brne delay1
+	; if debounce is set, return, else set debounce
+	lds debouncetemp, debounceflag1
+	cpi debouncetemp, 1
+	breq returnSetOne
+	ldi debouncetemp, 1
+	sts debounceflag1, debouncetemp
 	
-	;getPatternLength:
+	; return if activepatterns == 2
+	lds activepatternstemp, activepatterns
+	cpi activepatternstemp, 2
+	breq returnSetOne
+
+	; increment current pattern length
 	lds patternlengthtemp, patternindex
+	inc patternlengthtemp
+	sts patternindex, patternlengthtemp
 
 	addOneToPattern:
-		ldi temp1, 0b10000000
-		mov temp2, patternlengthtemp
-		rcall rightRotN			; temp1 = number to rotate, temp2 = number of times to rotate
+		ldi temp1, 0b00000001
+		;rcall rightRotN			; temp1 = number to rotate, temp2 = number of times to rotate
 		lds patterntemp, currentpattern
+		lsl patterntemp
 		or patterntemp, temp1
-
-	rcall storeNewPattern
-
-	pop patternlengthtemp
-	pop patterntemp
-	pop temp2
-	pop temp1
-	out SREG, temp1
-	pop temp1
-	reti
+		sts currentpattern, patterntemp
+	; if length of current word is pattern length
+	; increment stored patterns
+	; if no current active pattern, store pattern as current
+	cpi patternlengthtemp, patternlength
+	brlt returnSetOne
+	cpi activepatternstemp, 0
+	brne setOne_incActivePatterns
+	sts pattern, patterntemp
+	setOne_incActivePatterns:
+		inc activepatternstemp
+		sts activepatterns, activepatternstemp
+	returnSetOne:
+		pop patternlengthtemp
+		pop patterntemp
+		pop temp1
+		out SREG, temp1
+		pop temp1
+		reti
 
 setZero:
 	push temp1
 	in temp1, SREG	
 	push temp1
-	push temp2
 	push patterntemp
 	push patternlengthtemp
 
-	ldi temp1, 0xff
-	delayPB0: ; need to find a better way to handle buttons
-		dec temp1
-		cpi temp1, 0
-		brne delayPB0
-	ldi temp1, 0xff
-	delay1PB0: 
-		dec temp1
-		cpi temp1, 0
-		brne delay1PB0
+	; handle debounce
+	lds debouncetemp, debounceflag0
+	cpi debouncetemp, 1
+	breq returnSetZero
+	ldi debouncetemp, 1
+	sts debounceflag0, debouncetemp
 	
-	getPatternLength:
-		lds patternlengthtemp, patternindex
+	; handle active patterns
+	lds activepatternstemp, activepatterns
+	cpi activepatternstemp, 2
+	breq returnSetZero
+
+	; handle pattern length
+	lds patternlengthtemp, patternindex
+	inc patternlengthtemp
+	sts patternindex, patternlengthtemp
 
 	addZeroToPattern:
-		ldi temp1, 0b01111111
-		mov temp2, patternlengthtemp
-		rcall rightRotN			; temp1 = number to shift, temp2 = number of times to shift
 		lds patterntemp, currentpattern
+		lsl patterntemp
+		ldi temp1, 0b11111110
+		; rcall rightRotN			; temp1 = number to shift, temp2 = number of times to shift
 		and patterntemp, temp1
-		
-	rcall storeNewPattern
-		
-	pop patternlengthtemp
-	pop patterntemp
-	pop temp2
-	pop temp1
-	out SREG, temp1
-	pop temp1
-	reti
-
-; assume patterntemp, patternlengthtemp has been set
-storeNewPattern:
-	sts currentpattern, patterntemp
-	;out PORTC, patterntemp ; debug - display the pattern
-	inc patternlengthtemp
+		sts currentpattern, patterntemp
+	; if length of current word is pattern length
+	; increment stored patterns
+	; if no current active pattern, store pattern as current
 	cpi patternlengthtemp, patternlength
-	brlt storePatternLength
-	; reached a full new pattern
-	; if active patterns == 0, set pattern to currentpattern, clear currentpattern
-	; else leave as currentpattern
-	; inc active patterns
-	clr patternlengthtemp
-	lds activepatternstemp, activepatterns
+	brlt returnSetZero
 	cpi activepatternstemp, 0
-	brne incrementActivePatterns
+	brne setZero_incActivePatterns
 	sts pattern, patterntemp
-	ldi temp1, 0
-	sts currentpattern, temp1
-	incrementActivePatterns:
+	setZero_incActivePatterns:
 		inc activepatternstemp
 		sts activepatterns, activepatternstemp
-	storePatternLength:
-		sts patternindex, patternlengthtemp
-	ret
+	
+	returnSetZero:
+		pop patternlengthtemp
+		pop patterntemp
+		pop temp1
+		out SREG, temp1
+		pop temp1
+		reti
 
 timer0Interrupt:
 	push temp1
@@ -198,70 +168,83 @@ timer0Interrupt:
 	push yl
 	push temp2
 	push patterntemp
-	; return if no active patterns are ready to be displayed
-	lds temp1, activepatterns
-	cpi temp1, 0
-	breq returntimer0Interrupt
-	; increment the milisecond timer
-	lds yl, timercounter
-	lds yh, timercounter+1
-	adiw yh:yl, 1
-	; return if a second has not passed
-	cpi yl, low(timerinterval)
-	ldi temp1, high(timerinterval)
-	cpc yh, temp1
-	brlt storeTimer
+
+	; start debounce timers if either debounce is set
+	; otherwise set debounce timer and do nothing
+	lds debouncetemp, debounceflag0
+	cpi debouncetemp, 1
+	breq debounceHandle
+	lds debouncetemp, debounceflag1
+	cpi debouncetemp, 1
+	breq debounceHandle
+	rjmp testActivePatterns
+	debounceHandle:
+		rcall handleDebounceTimer
+	
+	testActivePatterns:
+		lds activepatternstemp, activepatterns
+		cpi activepatternstemp, 0
+		breq returntimer0Interrupt
+
+	incrementMili:
+		; increment the milisecond timer
+		lds yl, timercounter
+		lds yh, timercounter+1
+		adiw yh:yl, 1
+		; return if a second has not passed
+		cpi yl, low(timerinterval)
+		ldi temp1, high(timerinterval)
+		cpc yh, temp1
+		brlt storeTimer
+
 	; a second has passed
 	resetTimerCounter:
 		clr temp1
 		sts timercounter, temp1
 		sts timercounter+1, temp1
-	; toggle the pattern
+	; test the pattern
 
-	;lds temp1, pattern ; debug
-	;neg temp1
-	;sts pattern, temp1
-	;out portc, temp1
-	;jmp returntimer0Interrupt
-
-
-
-	; if numflashes == 0: turn on display
+	; toggle display
 	lds temp1, ispatterndisplayed
-	cpi temp1, 1
-	breq undisplayPattern
+	cpi temp1, 0
+	breq displayPattern
+	undisplayPattern:
+		ldi patterntemp, 0b00000000
+		out PORTC, patterntemp
+		ldi temp1, 0
+		sts ispatterndisplayed, temp1
+		rjmp incrementFlashesCounter
 	displayPattern:
 		lds patterntemp, pattern
 		out PORTC, patterntemp
 		ldi temp1, 1
 		sts ispatterndisplayed, temp1
-		rjmp incrementFlashesCounter
-	undisplayPattern:
-		lds patterntemp, 0b00000000
-		out PORTC, patterntemp
-		ldi temp1, 0
-		sts ispatterndisplayed, temp1
 		
 	incrementFlashesCounter:
-		lds flashcountertemp, flashcounter
-		inc flashcountertemp
-		cpi flashcountertemp, maxflashes
-		brlt storeFlashesCounter
+		;lds flashcountertemp, flashcounter
+		;inc flashcountertemp
+		;cpi flashcountertemp, maxflashes
+		;brlt storeFlashesCounter
 		; if max flashes reached:
 		; set flashes to zero
 		; decrement the activepatterns
 		; load the next pattern, if needed
-		clr flashcountertemp
-		lds temp1, activepatterns
-		subi temp1, 1
-		sts activepatterns, temp1
-		cpi temp1, 0
-		breq storeFlashesCounter
+		;lds temp1, activepatterns
+		;subi temp1, 1
+		;sts activepatterns, temp1
+		;cpi temp1, 0
+		;breq storeFlashesCounter
 		; load next pattern
+	changePattern:
+		clr flashcountertemp
+		clr temp1
 		lds patterntemp, currentpattern
 		sts pattern, patterntemp
-		clr temp1
 		sts currentpattern, temp1
+		sts patternindex, temp1
+		lds activepatternstemp, activepatterns
+		dec activepatternstemp
+		sts activepatterns, activepatternstemp
 		
 	storeFlashesCounter:
 		sts flashcounter, flashcountertemp
@@ -279,25 +262,69 @@ timer0Interrupt:
 		out SREG, temp1
 		pop temp1
 		reti
-	
-; Left rotates a number N times
-; params: number to shift, number of times to rotate: r20, r21
-; return r20
-rightRotN:
-	push r21
-	push r22 ; counter
-	clr r22
-	loop:
-		cp r22, r21
-		brge returnRightRotN
-		clc
-		ror r20
-		inc r22
-		rjmp loop
-	returnRightRotN:
-		pop r22
-		pop r21
+
+handleDebounceTimer:
+	push yl
+	push yh
+	push debouncetemp
+
+	lds yl, debouncecounter
+	lds yh, debouncecounter+1
+	adiw yh:yl, 1
+	; if less than debounce timer, do nothing
+	; else unset the debounce timer and clear the flags
+	cpi yl, low(debounceinterval)
+	ldi debouncetemp, high(debounceinterval)
+	cpc yh, debouncetemp
+	brlt storeDebounceTimer
+	clr yl
+	clr yh
+	sts debounceflag0, yl
+	sts debounceflag1, yl
+	storeDebounceTimer:
+		sts debouncecounter, yl
+		sts debouncecounter+1, yh
+
+	returnHandleDebounceTimer:
+		pop debouncetemp
+		pop yh
+		pop yl
 		ret
 
-wait:
-	rjmp wait
+main:
+	; clear all data variables
+	clr temp1
+	sts pattern, temp1
+	sts currentpattern, temp1
+	sts ispatterndisplayed, temp1
+	sts flashcounter, temp1
+	sts timercounter, temp1
+	sts debouncecounter, temp1
+	sts debounceflag0, temp1
+	sts debounceflag1, temp1
+
+	; set prescaling value of the timer
+	clr temp1
+	out TCCR0A, temp1
+	ldi temp1, 0b00000010
+	out TCCR0B, temp1
+	ldi temp1, 1<<TOIE0
+	sts TIMSK0, temp1
+
+	; set as falling edge for int1 and int0
+	ldi yh, high(EICRA)
+	ldi yl, low(EICRA)		
+	ldi temp1, ISC01
+	ori temp1, ISC11
+	st y, temp1 
+	; enable the interrupts
+	in temp1, EIMSK
+	ori temp1, (1<<INT0)
+	ori temp1, (1<<INT1)
+	out EIMSK, temp1
+	
+	sei
+	rjmp loop
+
+loop:
+	rjmp loop
