470e40f5eec67834b99d8126a51337a76be81224
Thomas Moffet <pantherman1996@gmail.com>
2016-05-23 09:24:31 +1000

    Added a small framework for doing the two main phases of the game. They don't use timers yet but they get random numbers that are needed. Also I made a divide and modulus function which will be useful for a few things should we need it.

--- Tom's Solution/Project/main.asm
+++ Tom's Solution/Project/main.asm
@@ -29,6 +29,8 @@ difficultyLevel: .byte 1
 .equ DIFFICULTY_HARD = 2
 .equ DIFFICULTY_REALLYHARD = 3
 
+currentStage: .byte 1
+
 currentRandomPotent: .byte 1 ; Or should this be two bytes?
 currentRandomCode: .byte 1
 randomCode1: .byte 1
@@ -73,14 +75,13 @@ Reset:
 ; Initialises the main variables.
 SetupMainVariables:
 	ldi r16, MODE_TITLESCREEN
-	ldi ZL, low(currentMode)
-	ldi ZH, high(currentMode)
-	st Z, r16
+	sts currentMode, r16
 
 	ldi r16, DIFFICULTY_EASY
-	ldi ZL, low(difficultyLevel)
-	ldi ZH, high(difficultyLevel)
-	st Z, r16
+	sts difficultyLevel, r16
+
+	ldi r16, 0
+	sts currentStage, r16
 
 	; currentRandomPotent, currentRandomCode and the randomCode variables are set
 	; when a button is pushed so that randomness can be decided.
@@ -98,7 +99,7 @@ Start:
 	out LED_OUT, r16
 	rjmp Halt
 
-; Displays the title screen of the game and waits for input.
+; Displays the title screen of the game.
 TitleScreen:
 	do_lcd_data '2'
 	do_lcd_data '1'
@@ -110,7 +111,7 @@ TitleScreen:
 	do_lcd_data 's'
 	do_lcd_data '1'
 
-	do_lcd_command 0b11000000
+	do_lcd_command LCD_SECONDLINE
 
 	do_lcd_data 'S'
 	do_lcd_data 'a'
@@ -127,7 +128,133 @@ TitleScreen:
 
 	ret
 
+; Starts the game section involving finding the right potential setting.
+; This creates a new random potential to find.
+StartFindPotent:
+	ldi r16, MODE_FINDPOTENT
+	sts currentMode, r16
+
+	lds r16, currentStage
+	inc r16
+	sts currentStage, r16
+
+	rcall GenerateNewRandomPotent
+	; TODO: Setup the timer.
+	; TODO: Setup another timer when the potentiometer is in the right place.
+	; TODO: Use some booleans to detect whether the user has entered or exited
+	; the spot.
+	ret
+
+; Starts the section that involves finding a code character.
+StartFindCode:
+	ldi r16, MODE_FINDCODE
+	sts currentMode, r16
+
+	rcall GenerateNewRandomCodeDigit
+
+	lds r16, currentStage
+	lds r17, currentRandomCode
+
+	cpi r16, 1
+	breq StartFindCode_StoreCode1
+	cpi r16, 2
+	breq StartFindCode_StoreCode2
+	rjmp StartFindCode_StoreCode3
+
+	StartFindCode_StoreCode1:
+		sts randomCode1, r17
+		rjmp StartFindCode_Search
+
+	StartFindCode_StoreCode2:
+		sts randomCode2, r17
+		rjmp StartFindCode_Search
+
+	StartFindCode_StoreCode3:
+		sts randomCode3, r17
+	StartFindCode_Search:
+		; TODO: Get the keypad input and confirm it's the key.
+		; TODO: Use a timer and poll whether the key is down in a loop.
+		; TODO: When the timer runs out and the key always was down, break.
+		; TODO: Otherwise, keep looping.
+
+	ret
+
+; Creates a new random potential and stores it.
+GenerateNewRandomPotent:
+	push r16
+	call CreateRandomValue
+	lds r16, currentRandomValue
+	; TODO: Figure out any refinements we need to make this work properly.
+	; (restrict it to a range?)
+	sts currentRandomPotent, r16
+	pop r16
+	ret
+	
+; Creates a new random digit and stores it.
+GenerateNewRandomCodeDigit:
+	push r16
+	push r17
+	push r18
+	call CreateRandomValue
+	lds r16, currentRandomValue
+	ldi r17, 10
+	rcall Divide
+	sts currentRandomCode, r18
+	pop r18
+	pop r17
+	pop r16.
+	ret
+
+.def argument = r16
+.def divisor = r17
+.def return = r18
+; Divides r16 by r17 and returns the result in r18.
+Divide:
+	push argument
+	push divisor
+	ldi return, 0
+	
+	Divide_Loop:
+		cp argument, divisor
+		brlt Divide_End
+		inc return
+		sub argument, divisor
+		rjmp Divide_Loop
+
+	Divide_End:
+		pop divisor
+		pop argument
+		ret
+
+.undef argument
+.undef divisor
+.undef return
+
+.def argument = r16
+.def modulo = r17
+.def return = r18
+; Moduloes r16 by r17 and returns the result in r18.
+Modulus:
+	push argument
+	push modulo
+
+	Modulus_Loop:
+	cp argument, modulo
+	brlt Modulus_End
+	sub argument, modulo
+	rjmp Modulus_Loop
+
+	Modulus_End:
+		mov modulo, argument
+		pop modulo
+		pop argument
+		ret
+
+.undef argument
+.undef modulo
+.undef return
+
 ; Stops the program.
-; Our program shouldn't really be in an ended state.
+; This only ever needs to be called on screens waiting for a push button.
 Halt:
 	rjmp Halt
